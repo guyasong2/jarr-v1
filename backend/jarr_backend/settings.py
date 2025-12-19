6610318324d614d88e2fbb1f82d711953f18d87d
Guy Asong <guyasong2@gmail.com>
2025-12-19 01:44:24 +0100

    Move Django SECRET_KEY to .env and ignore env files

--- backend/jarr_backend/settings.py
+++ backend/jarr_backend/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xm5fi&lw_53paf!z7k&@&!q^6omq=pmiu#g*rjcacifv*82s%p'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
